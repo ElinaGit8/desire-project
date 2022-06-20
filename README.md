123465c5875ae7ca98d0f48dd8785232ee37a44a
Elina Pakhomova <alwaysfollowyourdream8@gmail.com>
2022-06-20 16:46:32 +0300

    Create index.html

---
+++ README.md
@@ -0,0 +1,3 @@
+# desire-project 
+
+[Figma](https://www.figma.com/file/9Wvoo9PQ7L75M5PZrCoPvf/desire)
---
+++ app/js/main.js
@@ -0,0 +1,92 @@
+$(function () {
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
+  });
+
+  $(".rightside-menu__close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
+
+  $(".header__btn-menu").on("click", function () {
+    $(".menu").toggleClass("menu--open");
+  });
+
+  if ($(window).width() < 540) {
+    $(".works-path__item--measurements").appendTo($(".works-path__items-box"));
+  }
+
+  $(".top__slider").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 651,
+        settings: {
+          dots: false,
+        },
+      },
+    ],
+  });
+
+  $(".contact-slider").slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    centerPadding: 0,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 8,
+          slidesToScroll: 8,
+        },
+      },
+      {
+        breakpoint: 1511,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+        },
+      },
+      {
+        breakpoint: 1181,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 551,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
+
+  $(".article-slider__box").slick({
+    prevArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-left.svg" alt="arrow left"/></button>',
+    nextArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-right.svg" alt="arrow right"/></button>',
+  });
+
+  var mixer = mixitup(".gallery__inner", {
+    load: {
+      filter: ".category-bedroom",
+    },
+  });
+});
